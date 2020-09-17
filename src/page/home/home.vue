2e0e62464d3c01d181d7719a62d4a468682c4798
liu chang <aboutcroon@gmail.com>
2020-09-17 23:23:47 +0800

    home page init

---
+++ src/page/home/home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>home</div>
+</template>
+
+<script>
+import { cityGuess } from '../../service/getData'
+
+export default {
+  data() {
+    return {
+      guessCity: '', //当前城市
+      guessCityid: '', //当前城市id
+      hotcity: [], //热门城市列表
+      groupcity: {}, //所有城市列表
+    }
+  },
+  mounted() {
+    // 获取当前城市
+    cityGuess().then((res) => {
+      this.guessCity = res.name
+      this.guessCityid = res.id
+      console.log(res)
+      console.log(this.guessCity)
+    })
+  },
+}
+</script>
+
+<style>
+</style>
